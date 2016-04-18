3b54742eec0362b989a418ab6d18fd7c5ecccc97
Bruno Aguiar <brunoaguiar@ua.pt>
2016-04-18 17:38:22 +0100

    atualizacao

--- programa.py
+++ programa.py
@@ -1,5 +1,5 @@
-
 # encoding = utf-8
+
 import sys
 import select
 import json
@@ -8,23 +8,27 @@ from termcolor import colored, cprint
 
 def main():
 
-	# Config
+	# Socket
 	chat_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	chat_skt.bind(("0.0.0.0", 0))
 
-	#Ligar ao servidor
+	# Ligar ao servidor
 	endereco_server = chat_skt.connect( ("37.247.48.69", 1863) )
 
+	# Menu
 	print "\n"
 	print " --------------------------------------------------------------------------"
 	print "      				 CHAT - LABI                     "
 	print " --------------------------------------------------------------------------"
 	print(colored("\n 				  Conectado!\n", 'green'))
 
+	print " Seleciona uma opcao:\n 1 - Enviar mensagem\n 2 - Ajuda\n 3 - Sair\n"
+
 	msg_lista = []
 
 	while True:
 
+		# Select
 		msg_select = select.select([chat_skt, sys.stdin, ] , [], [])[0]
 		for sock in msg_select:
 			if sock == chat_skt:
@@ -42,23 +46,28 @@ def main():
 
 def mandar_msg(chat_skt, data):
 
-	utilizador = raw_input(colored('Escreva o seu nome de utilizador:  ', 'blue', attrs=['bold']) )
-	receptor = raw_input( colored('Enviar mensagem para  ', 'blue', attrs=['bold']))
-	mensagem = raw_input( colored('Mensagem:  ', 'blue', attrs=['bold']))
-	choose_user = {'from': utilizador}
-	data_flow={'from':utilizador, 'to':receptor, 'msg':mensagem}
+	menu = raw_input(colored("Opcao >> ", 'blue', attrs = ['bold']))
 	print "\n"
-	print colored('DE: ', 'cyan') + data_flow['from']
-	print colored('PARA: ', 'cyan') + data_flow['to']
-	if (not mensagem):
-		print colored("ERRO: Nao escreveste uma mensagem", 'red', attrs = ['bold'])
-	else:
-		print colored('MENSAGEM: ', 'cyan') + data_flow['msg']
+
+	if menu == "1":
+
+		utilizador = raw_input(colored('ESCOLHER NOME DE UTILIZADOR: ', 'blue', attrs=['bold']) )
+		receptor = raw_input( colored('ENVIAR MENSAGEM PARA: ', 'blue', attrs=['bold']))
+		mensagem = raw_input( colored('MENSAGEM: ', 'blue', attrs=['bold']))
+		choose_user = {'from': utilizador}
+		data_flow={'from':utilizador, 'to':receptor, 'msg':mensagem}
+		if (not mensagem):
+			print colored("ERRO: Nao escreveste uma mensagem\n", 'red', attrs = ['bold'])
+		else:
+			data = json.dumps(data_flow) + "\n"
+			chat_skt.send(data) #enviar resposta
 		print "\n"
-	data = json.dumps(data_flow) + "\n"
 
+	if menu == "2":
+		print colored("                                 	AJUDA:\n >> E' considerado erro quando: nao introduz mensagem ou quando recebem mensagem vazia.\n >> Se o campo 'ENVIAR MENSAGEM PARA' estiver vazio e' considerado que deseja mandar a mensagem para todos os utilizadores.\n", 'grey', attrs = ['bold'])
 
-	chat_skt.send(data) #enviar resposta
+	if menu == "3":
+		sys.exit()
 
 def receber_msg(chat_skt, data):
 
@@ -70,8 +79,9 @@ def receber_msg(chat_skt, data):
 			print colored('DE: ', 'green', attrs=['bold']) + d["from"]
 			print colored('PARA: ', 'green', attrs=['bold']) + d["to"]
 			print colored('MENSAGEM NOVA: ', 'green', attrs=['bold']) + d["msg"]
-		else:
-			print colored('ERRO: Mensagem do emissor nao encontrada). ', 'red')
+			print "\n"
+		elif "error" in d:
+			print colored('ERRO: Mensagem do emissor nao encontrada.\n ', 'red')
 	except:
 		men = data.split("\n")
 		try:
@@ -83,10 +93,10 @@ def receber_msg(chat_skt, data):
 					print colored('DE: ', 'green', attrs=['bold']) + d["from"]
 					print colored('PARA: ', 'green', attrs=['bold']) + d["to"]
 					print colored('MENSAGEM NOVA: ', 'green', attrs=['bold']) + d["msg"]
-				else:
-					print colored('ERRO: Mensagem do emissor nao encontrada). ', 'red')
+				elif "error" in d:
+					print colored('ERRO: Mensagem do emissor nao encontrada.\n ', 'red', attrs = ['bold'])
 		except:
-			while True:
+			while False:
 				continue
 
 
